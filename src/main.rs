5bf4906dd9e9008784454e3ce402db843d3842eb
timotree3 <timorcb@gmail.com>
2019-09-12 09:10:23 -0400

    Implement post creation

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,13 @@ use std::fs::File;
 use std::io::{ErrorKind, Read, Write};
 use std::path::{Path, PathBuf};
 
+fn base64_config() -> base64::Config {
+    base64::Config::new(base64::CharacterSet::UrlSafe, false)
+}
+
 fn path_for_blog(name: &str) -> PathBuf {
     let mut path = "state/blogs/".to_string();
-    base64::encode_config_buf(name, base64::Config::new(base64::CharacterSet::UrlSafe, false), &mut path);
+    base64::encode_config_buf(name, base64_config(), &mut path);
     dbg!(path.into())
 }
 
@@ -25,7 +29,10 @@ fn name_from_path<A: AsRef<Path>>(path: A) -> String {
         .expect("name_from_path called with invalid blog path")
         .to_str()
         .expect("base64 encoded blog name isn't valid utf8");
-    String::from_utf8(base64::decode_config(encoded, base64::Config::new(base64::CharacterSet::UrlSafe, false)).expect("blog name isn't valid base64")).expect("blog name isn't valid utf8")
+    String::from_utf8(
+        base64::decode_config(encoded, base64_config()).expect("blog name isn't valid base64"),
+    )
+    .expect("blog name isn't valid utf8")
 }
 
 #[derive(FromForm)]
@@ -36,7 +43,12 @@ struct BlogMeta {
 
 impl Display for BlogMeta {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        write!(f, "{}: {}", &self.name, self.description.as_ref().unwrap_or(&"".to_string()))
+        write!(
+            f,
+            "{}: {}",
+            &self.name,
+            self.description.as_ref().map(|x| &**x).unwrap_or("")
+        )
     }
 }
 
@@ -48,7 +60,12 @@ impl Display for BlogList {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         write!(f, "<ul>")?;
         for meta in &self.blogs {
-            write!(f, "<li><a href=\"{}\">{}</a></li>", uri!(blog_home: &meta.name), meta)?;
+            write!(
+                f,
+                "<li><a href=\"{}/\">{}</a></li>",
+                uri!(blog_home: &meta.name),
+                meta
+            )?;
         }
         write!(f, "</ul>")
     }
@@ -189,14 +206,107 @@ fn blogs() -> Content<String> {
         let meta = BlogMeta { name, description };
         bloglist.push(meta);
     }
-    Content(ContentType::HTML, format!(include!("blogs.html"), BlogList { blogs: bloglist } ))
+    Content(
+        ContentType::HTML,
+        format!(include!("blogs.html"), BlogList { blogs: bloglist }),
+    )
+}
+
+#[derive(FromForm)]
+struct DraftData {
+    title: String,
+    body: String,
+}
+
+#[get("/blogs/<name>/create")]
+fn create_post(name: String) -> Option<Content<&'static str>> {
+    let path = path_for_blog(&name);
+    if path.exists() {
+        Some(Content(ContentType::HTML, include_str!("createpost.html")))
+    } else {
+        None
+    }
+}
+
+fn path_for_post(name: &str, title: &str) -> PathBuf {
+    let b64_encoded = base64::encode_config(title, base64_config());
+    path_for_blog(name).join("posts").join(b64_encoded)
+}
+
+#[post("/blogs/<name>/create", data = "<submission>")]
+fn new_post_accept(
+    name: String,
+    submission: rocket::request::Form<DraftData>,
+) -> Result<Redirect, Flash<Redirect>> {
+    // create new folder with base64 encoded title
+    let post_path = path_for_post(&name, &submission.title);
+    match std::fs::create_dir(&post_path) {
+        Ok(()) => {}
+        Err(e) => match e.kind() {
+            ErrorKind::AlreadyExists => {
+                return Err(Flash::warning(
+                    Redirect::to(uri!(create_post: name)),
+                    "a post with that title already exists",
+                ))
+            }
+            ErrorKind::NotFound => {
+                return Err(Flash::error(
+                    Redirect::to(uri!(blogs)),
+                    "that blog doesn't exist or has been deleted",
+                ))
+            }
+            _ => panic!(
+                "failed to create dir for post. blog: {:?}, post_title: {:?}, error: {:?}",
+                name, submission.title, e
+            ),
+        },
+    }
+    // create file in folder called body.txt
+    let mut f = match File::create(post_path.join("body.txt")) {
+        Ok(f) => f,
+        Err(ref e) if e.kind() == ErrorKind::NotFound => {
+            return Err(Flash::error(
+                Redirect::to(uri!(blogs)),
+                "that blog has since been deleted",
+            ))
+        }
+        Err(e) => panic!(
+            "failed to create body.txt for post. blog: {:?}, post_title: {:?}, error: {:?}",
+            name, submission.title, e
+        ),
+    };
+    // write content into body.txt
+    match f.write_all(submission.body.as_bytes()) {
+        Ok(()) => {}
+        Err(ref e) if e.kind() == ErrorKind::NotFound => {
+            return Err(Flash::error(
+                Redirect::to(uri!(blogs)),
+                "that blog has since been deleted",
+            ))
+        }
+        Err(e) => panic!(
+            "failed to write to body.txt for post. blog: {:?}, post_title: {:?}, error: {:?}",
+            name, submission.title, e
+        ),
+    }
+    // return redirect to post link
+    // Ok(Redirect::to(uri!(view_post(name, title))))
+    Ok(Redirect::to(uri!(index)))
 }
 
 fn main() {
     rocket::ignite()
         .mount(
             "/",
-            routes![index, new_blog_form, new_blog_accept, blog_home, blogs],
+            routes![
+                index,
+                new_blog_form,
+                new_blog_accept,
+                blog_home,
+                blogs,
+                create_post,
+                new_post_accept
+            ],
         )
         .launch();
 }
